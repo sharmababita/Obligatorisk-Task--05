fbe39c647f6566302e8020ca41529badf35b71ff
sharmababita <sharma.089babita@gmail.com>
2025-07-14 23:28:41 +0200

    oldOne

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+async function getData() {
+    try {
+      const response = await fetch('https://catfact.ninja/fact');
+      const data = await response.json();
+      console.log(data); // Inspect structure
+    } catch (error) {
+      console.error("Something went wrong:", error);
+    }
+  }
+  
+  getData();
+  const container = document.querySelector('#data-container');
+  const fact = document.createElement('p');
+  fact.textContent = data.fact;
+  container.appendChild(fact);
+
+  
+  catch (error) {
+    const errorMsg = document.createElement('p');
+    errorMsg.textContent = "Something went wrong. Please try again later.";
+    container.appendChild(errorMsg);
+  }
+      
